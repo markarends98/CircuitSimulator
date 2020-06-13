bc61914ea307db7181908f76e1dfe866e672023d
Azur0 <bimre@avans.nl>
2020-06-13 21:48:25 +0200

    - removed unused usings

--- CircuitSimulator/ValidationStrategies/LoopValidation.cs
+++ CircuitSimulator/ValidationStrategies/LoopValidation.cs
@@ -3,11 +3,8 @@ using CircuitSimulator.Factories;
 using CircuitSimulator.Interfaces;
 using CircuitSimulator.Logs;
 using CircuitSimulator.Utils;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CircuitSimulator.ValidationStrategies
 {
